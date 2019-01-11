eebddc6acdada6f429b981b47b15f0870d09e683
PLPCap <46350322+PLPCap@users.noreply.github.com>
2019-01-11 14:10:27 +0530

    Add files via upload

---
+++ plp_version_9-master/FlyingJet/src/com/cg/plp/bean/BusinessSegmentBean.java
@@ -0,0 +1,23 @@
+package com.cg.plp.bean;
+
+public class BusinessSegmentBean {
+	
+	private String busSegId;
+	private String busSegName;
+	public String getBusSegId() {
+		return busSegId;
+	}
+	public void setBusSegId(String busSegId) {
+		this.busSegId = busSegId;
+	}
+	public String getBusSegName() {
+		return busSegName;
+	}
+	public void setBusSegName(String busSegName) {
+		this.busSegName = busSegName;
+	}
+	
+	
+	
+
+}
---
+++ plp_version_9-master/FlyingJet/src/com/cg/plp/exception/InsuranceException.java
@@ -0,0 +1,11 @@
+package com.cg.plp.exception;
+
+public class InsuranceException extends Exception 
+{
+
+	public InsuranceException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
